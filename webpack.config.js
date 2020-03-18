0c066933fdb1271c74d072903d4f985bfb999ea4
Thomas Röggla <tom@lichtfragmente.com>
2020-03-18 16:11:54 +0100

    added basic tsconfig and webpack build file

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: "./public/javascripts/main.ts",
+  },
+  output: {
+    path: __dirname + "./public/dist",
+    filename: "[name].js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      }, {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  }
+};
