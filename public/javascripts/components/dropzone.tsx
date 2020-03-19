ccc41a4cf4c25ff38d34a823d6015b5058fecd64
Thomas Röggla <tom@lichtfragmente.com>
2020-03-19 14:49:06 +0100

    added dropzone component for uploading files via drag and drop

---
+++ public/javascripts/components/dropzone.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { useState } from "react";
+import * as classNames from "classnames";
+
+interface DropzoneProps {
+  onFileDropped: (f: File) => void;
+}
+
+const Dropzone: React.FC<DropzoneProps> = (props) => {
+  const { onFileDropped } = props;
+  const [ dropzoneEntered, setDropzoneEntered ] = useState(false);
+
+  const parseFormData = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log("File dropped");
+    const file = e.dataTransfer.files.item(0);
+
+    if (file) {
+      onFileDropped(file);
+    }
+  }
+
+  return (
+    <div>
+      <div
+        className={classNames("dropzone", { "dropzone-entered": dropzoneEntered })}
+        onDragEnter={() => setDropzoneEntered(true)}
+        onDragLeave={() => setDropzoneEntered(false)}
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={parseFormData}
+      >
+        Drop a file here
+      </div>
+    </div>
+  );
+}
+
+export default Dropzone;
