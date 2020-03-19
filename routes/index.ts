10d4b4132c82d58f764a74c7f427bf6aa4866fd6
Thomas Röggla <tom@lichtfragmente.com>
2020-03-19 21:36:41 +0100

    implemented /register route

--- routes/index.ts
+++ routes/index.ts
@@ -1,12 +1,28 @@
 import { Router } from "express";
+import { getDatabase, hashPassword } from "../util";
+
 const router = Router();
 
 router.get("/", (req, res, next) => {
   res.render("index", { title: "TRACTION" });
 });
 
-router.get("/register", (req, res) => {
-  const { username, password, confirm } = req.body;
+router.post("/register", async (req, res) => {
+  const db = await getDatabase(req);
+  const { username, password, confirmation } = req.body;
+
+  if (password === confirmation) {
+    const users = db.collection("users");
+    const hash = hashPassword(password);
+
+    await users.insertOne({
+      username, hash
+    });
+
+    res.send({ status: "OK" });
+  } else {
+    res.send({ status: "ERR" });
+  }
 });
 
 router.get("/login", (req, res) => {
