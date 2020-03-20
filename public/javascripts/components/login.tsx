c59ab580a39da0d124583dfcc0cc912d73b0147c
Thomas Röggla <tom@lichtfragmente.com>
2020-03-20 19:59:26 +0100

    added component for handling logins

---
+++ public/javascripts/components/login.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { useState } from "react";
+
+interface LoginProps {
+  loginSuccess: () => {};
+}
+
+const Login: React.FC<LoginProps> = (props) => {
+  const { loginSuccess } = props;
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async () => {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+    });
+
+    if (response.ok) {
+      loginSuccess();
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <input className="input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input className="input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button
+          disabled={username.length === 0 || password.length === 0}
+          onClick={onSubmit}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login
