4f110f7ba0aaf9f6de8dde3c8701b68c88029618
Thomas Röggla <tom@lichtfragmente.com>
2020-03-20 01:19:29 +0100

    added basic router to app component

--- public/javascripts/components/app.tsx
+++ public/javascripts/components/app.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 interface AppProps {
 }
 
 const App: React.FC<AppProps> = () => {
   return (
-    <div>Hello World</div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <div>Hello World</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
