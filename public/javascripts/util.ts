ee38982c5a40f15b4f8cd620402b30f9c72c9cc5
Thomas Röggla <tom@lichtfragmente.com>
2020-03-29 20:21:39 +0200

    added param for progress updates to postFile function

--- public/javascripts/util.ts
+++ public/javascripts/util.ts
@@ -1,9 +1,10 @@
-export function postFile(endpoint: string, file: File, field = "file") {
+export function postFile(endpoint: string, file: File, onProgress: (e: ProgressEvent) => void, field = "file") {
   const formData = new FormData();
   formData.append(field, file);
 
   const xhr = new XMLHttpRequest();
 
+  xhr.upload.onprogress = onProgress;
   xhr.open("POST", endpoint);
   xhr.send(formData);
 
