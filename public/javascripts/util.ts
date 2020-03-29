0878ffae742784f40900c7aae4acc902e2a5165c
Thomas Röggla <tom@lichtfragmente.com>
2020-03-29 23:11:31 +0200

    postFile returns promise which resolves upon upload completion

--- public/javascripts/util.ts
+++ public/javascripts/util.ts
@@ -3,10 +3,20 @@ export function postFile(endpoint: string, file: File, onProgress: (e: ProgressE
   formData.append(field, file);
 
   const xhr = new XMLHttpRequest();
-
   xhr.upload.onprogress = onProgress;
+
+  const promise = new Promise((resolve, reject) => {
+    xhr.upload.onloadend = () => {
+      resolve();
+    }
+
+    xhr.upload.onerror = (err) => {
+      reject(err);
+    };
+  });
+
   xhr.open("POST", endpoint);
   xhr.send(formData);
 
-  return xhr.upload;
+  return promise;
 }
