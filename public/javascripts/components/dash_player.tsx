2ae32c560a12c91197f8a3dbbee71061d7ab6f6e
Thomas Röggla <tom@lichtfragmente.com>
2020-03-29 19:01:33 +0200

    including video.js CSS from package itself

--- public/javascripts/components/dash_player.tsx
+++ public/javascripts/components/dash_player.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useEffect, useRef } from "react";
 import videojs from "video.js";
 
+import "video.js/dist/video-js.css";
 import "videojs-contrib-dash";
 
 interface DashPlayerProps {
